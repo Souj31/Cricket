c25d4f4ee95238dd0a7b3ff15b8c03d968075762
soujanya <soujanya.g@perfios.com>
2022-08-27 16:07:30 +0530

    Java

---
+++ Cricket.java
@@ -0,0 +1,11 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.*;
+import java.util.Map;
+
+public class Cricket {
+
+    public static void main(String[] args) {
+        List<String,Integer> listNames = new ArrayList<>();
+    }
+}
